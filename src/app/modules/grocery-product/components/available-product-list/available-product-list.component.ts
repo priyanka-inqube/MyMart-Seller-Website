0f4fb1a1ba815496eaac461af6f16cb0a26c0e3f
priyanka.inqube <priyanka.inqube@gmail.com>
2021-08-20 21:35:05 +0530

    left side category fixed

--- src/app/modules/grocery-product/components/available-product-list/available-product-list.component.ts
+++ src/app/modules/grocery-product/components/available-product-list/available-product-list.component.ts
@@ -14,7 +14,7 @@ export class AvailableProductListComponent implements OnInit {
   constructor(private __category:CategoryService) { }
   ngOnInit(): void {
     $(document).ready(function(){$(".dash-product-item .graphic_thumb_caro").slick()});
-    this.groceryLevelCategory()
+    // this.groceryLevelCategory()
   }
 
   groceryLevelCategory(){
